daabc8c2a9e59b47d05065095cbb19fc6580b69c
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-30 21:22:57 +0300

    moving the file to Battleships folder

---
+++ src/main/Battleships/AI.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class AI {
+
+    public int[] chooseCell() {
+
+        Random random = new Random();
+        int InputRowAi = (random.nextInt(10));
+        int InputColumnAi = (random.nextInt(10));
+
+        int[] InputAi = new int[2];
+        InputAi[0] = InputRowAi;
+        InputAi[1] = InputColumnAi;
+
+        return InputAi;
+    }
+}

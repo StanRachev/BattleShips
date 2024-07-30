dd4893774309079c27462eb81caeebd1585bc832
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-30 21:32:07 +0300

    removed the ui() method

---
+++ src/main/Battleships/BattleShips.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class BattleShips {
+
+    private static boolean isNewGame = true;
+
+    public static void main(String[] args) {
+
+        UI ui = new UI();
+        ui.intro();
+        ui.chooseGameMode();
+
+        int shipPlacement = ui.placeYourShips();
+        GameManager myGame = new GameManager();
+        while (isNewGame) {
+
+            boolean isGameOver = false;
+            while (!isGameOver) {
+                myGame.launchGame(shipPlacement);
+                isGameOver = myGame.isGameOver();
+            }
+
+            if (myGame.playerWins()) {
+                System.out.println("\nYou win!");
+            } else {
+                System.out.println("\nGame over");
+            }
+            anotherGame();
+        }
+    }
+
+    public static void anotherGame() {
+
+        System.out.println("\nDo you want another game?");
+        System.out.print("Y/N: ");
+
+        char userInput = 'V';
+        boolean isRightLetter = false;
+        while (!isRightLetter) {
+            Scanner scan = new Scanner(System.in);
+            userInput = scan.next().charAt(0);
+            if (Character.toUpperCase(userInput) == 'N' || Character.toUpperCase(userInput) == 'Y') {
+                isRightLetter = true;
+            }
+        }
+        if (Character.toUpperCase(userInput) == 'N') {
+            isNewGame = false;
+        }
+        GameManager.wipeScreen();
+    }
+}
